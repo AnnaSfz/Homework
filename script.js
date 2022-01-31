151da6d3d7fa614c556c169533864ea9d98e4012
AnnaSfz <anna.lomachynska@gmail.com>
2022-01-31 22:44:12 +0200

    add hw

---
+++ script.js
@@ -0,0 +1,20 @@
+// let userAge = '32';
+// let userName = 'John';
+// let userSurname = 'Brown';
+
+// let newUser;
+// let internetShopCard;
+// let userOnline;
+// let employeeWage;
+// let cursorCoordinates;
+
+
+let message = 'Hello World!';
+console.log(message);
+
+let userOnline = 'userOnline';
+console.log(userOnline);
+
+
+const winMessage = 'Congratulations! You won!'
+alert(winMessage);
