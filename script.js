200174818aa597c8e9f104cbc1502009d59e2d80
AnnaSfz <anna.lomachynska@gmail.com>
2022-02-15 23:08:29 +0200

    changes

--- script.js
+++ script.js
@@ -10,30 +10,40 @@
 //     alert(`Числа равны`);
 // }
 
-const num1 = Number(prompt("Enter number1"));
-const action = prompt("Enter +, -, * or /");
-const num2 = Number(prompt("Enter number2"));
- let result;
+// const num1 = Number(prompt("Enter number1"));
+// const action = prompt("Enter +, -, * or /");
+// const num2 = Number(prompt("Enter number2"));
+//  let result;
 
- switch (action) {
-     case "+":
-     result = num1+num2;
-     break;
+//  switch (action) {
+//      case "+":
+//      result = num1+num2;
+//      break;
 
-     case "-":
-     result = num1-num2;
-     break;
+//      case "-":
+//      result = num1-num2;
+//      break;
 
-     case "*":
-     result = num1*num2;
-     break;
+//      case "*":
+//      result = num1*num2;
+//      break;
 
-     case "/":
-     result = num1/num2;
-     break;
+//      case "/":
+//      result = num1/num2;
+//      break;
 
-     default:
-         result = "Error"
+//      default:
+//          result = "Error"
 
- }
- alert (result);
+//  }
+//  alert (result);
+
+const userAge = Number(prompt("Age: "));
+
+if ((userAge >= 12 && userAge < 18) || (userAge >= 60 && userAge < 80))
+const isParents = confirm("Взрослые дали разрешение?")
+if (isParents){
+    alert("Можете проходить");
+} else {
+    alert("Запрещено");
+}
