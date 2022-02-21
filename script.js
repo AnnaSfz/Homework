196ec4d9dbad923baf456de1a7991e4ca011f26c
AnnaSfz <anna.lomachynska@gmail.com>
2022-02-21 23:49:18 +0200

    homework

--- script.js
+++ script.js
@@ -52,3 +52,50 @@
 // } else {
 //     alert("Можете проходить");
 // }
+// function showMessage () {
+//     console.log ("Привет из функции!");
+// }
+//  showMessage ();
+//  showMessage ();
+// function getSum () {
+//     const num1 = Number(prompt("Первое число"));
+//     const num2 = Number(prompt("Второе число"));
+//     const sum = num1 + num2;
+//     alert("Сумма: " + sum);
+// }
+// getSum()
+// getSum()
+// let userName =  "";
+// function getUserName () {
+//     console.log (`userName`, userName);
+//     if (!userName) {
+//         userName = prompt ("What is your name?")
+//     }
+// }
+// getUserName ();
+// console.log(`userName`, userName);
+
+// let login = "user1";
+// function getLogin () {
+//     let login = "admin2";
+//     login = prompt ("Enter name");
+//     console.log (`login local`, login);
+// }
+// getLogin ();
+// console.log (`login global`, login);
+
+
+let name = prompt('What is your name?');
+function checkAge(age) {
+    if (age > 30){
+        return 'Good afternoon';
+    } else (age < 30) {
+        return 'Hi';
+    }
+}
+let age  = prompt('How old are you?');
+if ( checkAge(age)) {
+    alert( 'Good afternoon ' );
+} else {
+    alert ( 'Hi ');
+}
