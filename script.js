11547df0e546601ffe9182ee1d13425a747b653c
AnnaSfz <anna.lomachynska@gmail.com>
2022-02-22 01:37:23 +0200

    homework.

--- script.js
+++ script.js
@@ -84,18 +84,42 @@
 // getLogin ();
 // console.log (`login global`, login);
 
+//   Второе задание в домашке 4 
+// let name = prompt('What is your name?');
+// function checkAge(age) {
+//     if (age > 30){
+//         return 'Good afternoon';
+//     } else (age < 30) {
+//         return 'Hi';
+//     }
+// }
+// let age  = prompt('How old are you?');
+// if ( checkAge(age)) {
+//     alert( 'Good afternoon ' );
+// } else {
+//     alert ( 'Hi ');
+// }
+//  Первое задание в домашке 4
+// function getSum() {
+//     const num1 = Number(prompt("First number"));
+//     const num2 = Number(prompt("Second number"));
+//     const sum = num1+num2;
+//     alert("Сумма: " + sum);
+// }
+// getSum()
+// getSum()
 
-let name = prompt('What is your name?');
-function checkAge(age) {
-    if (age > 30){
-        return 'Good afternoon';
-    } else (age < 30) {
-        return 'Hi';
-    }
-}
-let age  = prompt('How old are you?');
-if ( checkAge(age)) {
-    alert( 'Good afternoon ' );
-} else {
-    alert ( 'Hi ');
-}
+// const sum = (a, b) => a + b;
+// const sum2 = (c, d) => c + d;
+//     const res1 = sum(10, 10)
+//     const res2 = sum2(20, 30)
+//     console.log('res1', res1)
+//     console.log('res2', res2)
+
+// 
+// function multiply(a, b) {
+//     return a*b;
+//   }
+  
+//   multiply(5, 2); 
+//   console.log(10)
