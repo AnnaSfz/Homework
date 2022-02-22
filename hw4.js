2b16d88b7601eab25c2c511b7638530921438806
AnnaSfz <anna.lomachynska@gmail.com>
2022-02-23 00:01:13 +0200

    add changes

---
+++ hw4.js
@@ -0,0 +1,41 @@
+
+//   Второе задание в домашке 4 
+// let name = prompt('What is your name?');
+// function checkAge(age) {
+//     if (age > 30){
+//         return 'Good afternoon';
+//     } else (age < 30) {
+//         return 'Hi';
+//     }
+// }
+// let age  = prompt('How old are you?');
+// if ( checkAge(age)) {
+//     alert( 'Good afternoon ' );
+// } else {
+//     alert ( 'Hi ');
+// }
+//  Первое задание в домашке 4
+// function getSum() {
+//     const num1 = Number(prompt("First number"));
+//     const num2 = Number(prompt("Second number"));
+//     const sum = num1+num2;
+//     alert("Сумма: " + sum);
+// }
+// getSum()
+// getSum()
+
+// const sum = (a, b) => a + b;
+// const sum2 = (c, d) => c + d;
+//     const res1 = sum(10, 10)
+//     const res2 = sum2(20, 30)
+//     console.log('res1', res1)
+//     console.log('res2', res2)
+
+//  Третье задание
+
+// function multiply(a, b) {
+//     return a*b;
+//   }
+  
+//   multiply(5, 2); 
+//   console.log(10)
